0ccaa3e4eb5fa0f33d2b3c6c2e62a4cce7572a4b
ahmad kholilur r <akhomantea55@gmail.com>
2026-01-28 12:47:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from yt_dlp import YoutubeDL
 import os
-import uuid
 
 app = Flask(__name__)
 
@@ -12,11 +11,10 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 def index():
     return render_template("index.html")
 
-@app.route("/prepare", methods=["POST"])
-def prepare():
-    url = request.form["url"]
-    file_id = str(uuid.uuid4())
-    filename = f"{file_id}.mp4"
+@app.route("/download")
+def download():
+    url = request.args.get("url")
+    filename = "video.mp4"
     filepath = os.path.join(DOWNLOAD_DIR, filename)
 
     ydl_opts = {
@@ -28,11 +26,6 @@ def prepare():
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-    return f"/download/{filename}"
-
-@app.route("/download/<filename>")
-def download(filename):
-    filepath = os.path.join(DOWNLOAD_DIR, filename)
     return send_file(filepath, as_attachment=True)
 
 if __name__ == "__main__":
